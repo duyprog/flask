8dcd9dc8767dd9cd2948d06bcc298580619ca075
duy-prog-2k <phamkhacduy.it@gmail.com>
2022-01-22 10:28:40 +0700

    add code

---
+++ RestAPI/Section5/code/item.py
@@ -0,0 +1,109 @@
+import sqlite3
+from flask_restful import Resource, reqparse
+from flask_jwt import jwt_required
+
+class Item(Resource):
+
+    parser = reqparse.RequestParser() 
+    parser.add_argument('price', 
+        type=float, 
+        required=True,
+        help="This field cannot be bank !"
+    )
+
+    @classmethod
+    def find_by_name(cls, name):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = 'SELECT * FROM items WHERE name=?'
+        result = cursor.execute(query, (name,))
+        row = result.fetchone()
+        connection.close()
+
+        if row: 
+            return {'item': {'name': row[0], 'price': row[1]}}
+    
+    @classmethod
+    def insert(cls, item):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+        query = "INSERT INTO items VALUES (?, ?)"
+        cursor.execute(query, (item['name'],item['price']))
+
+        connection.commit() 
+        connection.close()
+
+    @classmethod
+    def update(cls, item):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+        
+        query = 'UPDATE items SET price=? WHERE name = ?'
+        cursor.execute(query, (item['price'], item['name']))
+        
+        connection.commit() 
+        connection.close()
+        pass   
+
+    @jwt_required() # must authenticate before call get method, when test api, modify content-type in header tab to application/json
+    def get(self, name):
+        item = self.find_by_name(name)
+
+        if item: 
+            return item, 200
+        return {'message': 'Item not found'}, 404 
+
+
+    @jwt_required()
+    def post(self, name):
+        if self.find_by_name(name):
+            return {'message': 'Item is already exists !'}
+
+        data = Item.parser.parse_args()
+        
+        item = {'name': name, 'price': data['name']}
+        self.insert(item)
+        return item
+        
+    def put(self, name): 
+        data = Item.parser.parse_args() 
+         
+        update_item = {'name': name, 'price': data['price']}
+        item = self.find_by_name(name)
+
+        if item is None: 
+            try: 
+                Item.insert(update_item)
+            except:
+                return {'message': 'An error occurred inserting the item'}
+        else: 
+            try: 
+                Item.update(update_item)
+            except: 
+                return {'message': 'An error occurred inserting the item'} 
+        return update_item
+
+
+    @jwt_required()
+    def delete(self, name):
+        if self.find_by_name(name):
+            connection = sqlite3.connect('data.db')
+            cursor = connection.cursor() 
+
+            query = 'DELETE FROM items WHERE name=?'
+            cursor.execute(query, (name,))
+            connection.commit()
+            connection.close()
+
+            return {'message': 'Item deleted'}, 200
+
+        return {'message': 'Item not found'}, 404
+
+    
+
+# pass
+
+# class ItemList(Resource):
+#     def get(self):
+#         return {'item': items}
--- RestAPI/Section5/code/user.py
+++ RestAPI/Section5/code/user.py
@@ -1,6 +1,7 @@
+from asyncio import QueueEmpty
 import sqlite3
-
-
+from flask_restful import Resource, reqparse
+from flask import  request
 class User:
     def __init__(self, _id, username, password):
         self.id = _id
@@ -38,3 +39,34 @@ class User:
         
         connection.close() # if add any data we need to commit 
         return user
+
+class UserRegister(Resource):
+
+    parser = reqparse.RequestParser()
+    parser.add_argument('username',
+        type=str, 
+        required=True, 
+        help="This field cannot be blank !"
+    )
+
+    parser.add_argument('password', 
+        type=str, 
+        required=True, 
+        help="You must enter the password!"
+    )
+
+    def post(self): 
+        data = UserRegister.parser.parse_args()
+
+        if User.find_by_username(data['username']):
+            return {'message': 'User is already exists !'}, 400
+        else:
+            connection = sqlite3.connect('data.db')
+            cursor = connection.cursor() 
+
+            query = 'INSERT INTO users VALUES (NULL, ?, ?)'
+            cursor.execute(query, (data['username'], data['password']))
+            connection.commit()
+            connection.close()  
+
+        return {'message': 'User create succcessfully'}, 201 
